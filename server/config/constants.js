f7f8f509fec54112ed1c98383ddeef5e23035e66
Itoshi-web <ffgarageofficial@gmail.com>
2024-12-31 18:31:40 +0530

    Update constants.js

--- server/config/constants.js
+++ server/config/constants.js
@@ -14,7 +14,7 @@ export const EVENTS = {
 };
 
 export const ORIGINS = [
-  'https://lambent-nasturtium-dbb11c.netlify.app',
+  'https://shimmering-centaur-6d9e56.netlify.app/',
   'http://localhost:5173'
 ];
 
